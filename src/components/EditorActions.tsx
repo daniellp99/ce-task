3ca0fa4874c62c3cf00c606b9e13a545252235d8
Daniel Alejandro Leyva Perez <82961309+daniellp99@users.noreply.github.com>
2024-06-30 10:18:17 -0400

    feat: disable form actions when form is submitting

--- src/components/EditorActions.tsx
+++ src/components/EditorActions.tsx
@@ -5,44 +5,69 @@ import {
   MoveDiagonalIcon,
   SunIcon,
 } from "lucide-react";
-import { UseFormReturn } from "react-hook-form";
+import { UseFormReset } from "react-hook-form";
 
 import { Button } from "@/components/ui/button";
 
 import { TaskType } from "@/lib/schemas";
+import { cn } from "@/lib/utils";
 
 export default function EditorActions({
-  form,
+  hasChanges,
+  pending,
+  showActions,
+  resetForm,
 }: {
-  form: UseFormReturn<TaskType>;
+  hasChanges: boolean;
+  pending: boolean;
+  showActions: boolean;
+  resetForm: UseFormReset<TaskType>;
 }) {
-  const hasChanges = form.formState.isDirty;
-
   return (
     <fieldset
       id="actions"
-      className="flex items-center justify-between gap-8 text-sm h-14 p-2 bg-gray-50"
+      className={cn(
+        " items-center justify-between gap-8 text-sm bg-gray-50 h-14 p-2",
+        showActions ? "flex" : "hidden"
+      )}
     >
       <div>
         <Button
           variant="secondary"
-          disabled={!hasChanges}
+          type="button"
+          disabled={!hasChanges || pending}
           className="hover:bg-secondary text-slate-900 disabled:text-slate-600"
         >
           <MoveDiagonalIcon className="mr-2 size-4 stroke-slate-600" /> Open
         </Button>
       </div>
       <div className="grow items-start flex gap-1">
-        <Button variant="action" disabled={!hasChanges}>
+        <Button
+          variant="action"
+          type="button"
+          disabled={!hasChanges || pending}
+        >
           <CalendarIcon className="mr-2 size-4" /> Today
         </Button>
-        <Button variant="action" disabled={!hasChanges}>
+        <Button
+          variant="action"
+          type="button"
+          disabled={!hasChanges || pending}
+        >
           <LockOpenIcon className="mr-2 size-4" /> Public
         </Button>
-        <Button variant="action" disabled={!hasChanges}>
+        <Button
+          variant="action"
+          type="button"
+          disabled={!hasChanges || pending}
+        >
           <SunIcon className="mr-2 size-4" /> Highlight
         </Button>
-        <Button variant="action" disabled={!hasChanges}>
+        <Button
+          variant="action"
+          type="button"
+          disabled={!hasChanges || pending}
+        >
           <CircleDotIcon className="mr-2 size-4" /> Estimation
         </Button>
       </div>
@@ -50,12 +75,17 @@ export default function EditorActions({
         <Button
           type="button"
           variant="secondary"
+          disabled={pending}
           className="bg-slate-100 hover:bg-slate-100"
-          onClick={() => form.reset()}
+          onClick={() => resetForm()}
         >
           Cancel
         </Button>
-        <Button type="submit" className="bg-blue-700 hover:bg-blue-700">
+        <Button
+          type="submit"
+          disabled={pending}
+          className="bg-blue-700 hover:bg-blue-700"
+        >
           {hasChanges ? "Add" : "Ok"}
         </Button>
       </div>
