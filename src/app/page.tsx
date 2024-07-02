a4bc9667312542224f7d88d3535553a637f7a640
Daniel Alejandro Leyva Perez <82961309+daniellp99@users.noreply.github.com>
2024-07-02 13:42:34 -0400

    fix: add space between items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import EditTaskForm from "@/components/EditTaskForm";
 export default async function Home() {
   const tasks = await getTasksAction();
   return (
-    <main className="min-h-screen container mt-14">
+    <main className="min-h-screen container mt-14 flex flex-col gap-2 items-left justify-start">
       <CreateTaskForm />
       {tasks.map((task) => (
         <EditTaskForm key={task.id} task={task} />
