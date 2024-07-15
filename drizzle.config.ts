4c41b3fd6ee5b90b13f934de3e6358cf400b5d87
Daniel Alejandro Leyva Perez <82961309+daniellp99@users.noreply.github.com>
2024-07-15 09:43:43 -0400

    fix: use turso  as  sqlite provider for vercel deployment

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,10 @@ import { type Config } from "drizzle-kit";
 export default {
   schema: "./src/server/db/schema.ts",
   dialect: "sqlite",
+  driver: "turso",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
+    authToken: process.env.DATABASE_AUTH_TOKEN,
   },
   tablesFilter: ["ce_task_*"],
 } satisfies Config;
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -12,7 +12,11 @@ const globalForDb = globalThis as unknown as {
 };
 
 export const client =
-  globalForDb.client ?? createClient({ url: process.env.DATABASE_URL! });
+  globalForDb.client ??
+  createClient({
+    url: process.env.DATABASE_URL!,
+    authToken: process.env.DATABASE_AUTH_TOKEN,
+  });
 if (process.env.NODE_ENV !== "production") globalForDb.client = client;
 
 export const db = drizzle(client, { schema });
